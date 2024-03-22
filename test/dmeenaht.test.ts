97b79b40388b754829a8ef68c01f5da075ad01f3
Paponthanai Ounsopa <71958540+neonaht@users.noreply.github.com>
2024-03-22 20:46:54 +0700

    Initial commit

---
+++ test/dmeenaht.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Dmeenaht from '../lib/dmeenaht-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dmeenaht-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Dmeenaht.DmeenahtStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
